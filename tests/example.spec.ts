19d28e2bb9902ec75ee412f467cee9a125f9a5f4
Andy Neville <1382767+andyneville@users.noreply.github.com>
2024-07-17 09:31:55 -0700

    remove percy

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,6 +1,5 @@
 // import { test, expect } from '@playwright/test';
 import { test, expect } from "@chromatic-com/playwright";
-import percySnapshot from '@percy/playwright';
 
 test('has title', async ({ page }) => {
   await page.goto('http://localhost:5173/');
@@ -17,5 +16,4 @@ test('increment count', async ({ page }) => {
   await page.getByRole('button', { name: 'count is 0' }).click();
 
   await expect(page.getByRole('button', { name: 'count is 1' })).toBeVisible();
-  await percySnapshot(page, 'Example Site');
 });
