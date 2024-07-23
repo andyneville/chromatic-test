ce6233bb9cd476e154653220181c553b10b501bf
Andy Neville <1382767+andyneville@users.noreply.github.com>
2024-07-23 09:47:22 -0700

    new build working, but Chromatic server not detecting differences

--- src/App.tsx
+++ src/App.tsx
@@ -18,8 +18,8 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button className='text-xl' onClick={() => setCount((count) => count + 1)}>
+          Count is {count}
         </button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
